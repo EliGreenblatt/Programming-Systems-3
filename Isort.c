7930a48c5bb5d2334c4dcc19603c0c86efbef77c
EliGreenblatt <eligreenblatt9@gmail.com>
2022-12-25 15:49:21 +0200

    added methods to check functionality

--- Isort.c
+++ Isort.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+
+#define SIZE 50
+
 void shift_element(int* arr, int i);
 void insertion_sort(int* arr , int len);
+void enterNumbers(int * arr , int size);
+void printNumbers(int * arr , int size);
 
 
 int main()
 {
-    int arr[10] = {10,9,8,7,6,5,4,3,2,1};
-    
-    insertion_sort(arr , 10);
-    
+    // we define the array , then simply enter input from the user into the array
+    // we then implement on the array insertion sort , then finally print to check for success
+    int arr[SIZE] = {0}; 
+    enterNumbers(arr , SIZE);
+    insertion_sort(arr , SIZE);
+    printNumbers(arr , SIZE);
 
     return 1;
 }
 
+
+// side-function shifts from arr pointer , i amount of indexes to the right
 void shift_element(int* arr, int i)
 {
     for (int j = i; j > 0; j--)
@@ -21,6 +30,7 @@ void shift_element(int* arr, int i)
     }
 }
 
+// implementation of insertion sort using the shift method
 void insertion_sort(int* arr , int len)
 {
     for (int i = 0; i < len - 1; i++)
@@ -37,15 +47,28 @@ void insertion_sort(int* arr , int len)
                     *(arr + j) = key;
                     break;
                 }
-            }
-              
+            }   
         }
+    }
+    
+}
 
-        for (int i = 0; i < 10; i++)
-     {
-             printf("%d," , arr[i]);
-     }
-            printf("\n");
+// we simply print the numbers to check for success
+void enterNumbers(int * arr , int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        int c = 0;
+        scanf(" %d"  , &c);
+        *(arr + i) = c;
+    }  
+}
+
+void printNumbers(int * arr , int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d,", *(arr + i));
     }
     
 }
